232a3e78a43d0f8cded72d82c5faf94225d09652
sun1991 <sun1991@126.com>
2017-02-05 15:16:42 +0800

    find longest increasing subsequence in progress

---
+++ mylintcodesolution/longest_increasing_subsequence.py
@@ -0,0 +1,13 @@
+# http://www.lintcode.com/en/problem/longest-increasing-subsequence/
+
+
+#For [5, 4, 1, 2, 3], the LIS is [1, 2, 3], return 3
+#For [4, 2, 4, 5, 3, 7], the LIS is [2, 4, 5, 7], return 4
+
+class Solution:
+    """
+    @param nums: The integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        pass
