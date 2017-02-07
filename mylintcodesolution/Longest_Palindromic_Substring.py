f1a83f2790f1a109e9fb9ad0515edef95e3d75a5
sun1991 <sun1991@126.com>
2017-02-07 15:52:42 +0800

    Longest Palindromic Substring  in progress

---
+++ mylintcodesolution/Longest_Palindromic_Substring.py
@@ -0,0 +1,8 @@
+# http://www.lintcode.com/en/problem/longest-palindromic-substring/
+
+# Given the string = "abcdzdcab", return "cdzdc"
+class Solution:
+    # @param {string} s input string
+    # @return {string} the longest palindromic substring
+    def longestPalindrome(self, s):
+        pass
