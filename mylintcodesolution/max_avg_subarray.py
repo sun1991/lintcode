0a19eb52917e8eeeb98365d9591179ac6e65a4de
sun1991 <sun1991@126.com>
2017-02-08 12:46:55 +0800

    exceed time limit...

--- mylintcodesolution/max_avg_subarray.py
+++ mylintcodesolution/max_avg_subarray.py
@@ -4,7 +4,7 @@
 #Given nums = [1, 12, -5, -6, 50, 3], k = 3
 #Return 15.667 // (-6 + 50 + 3) / 3 = 15.667
 
-class Solution_OLD:
+class Solution:
     # @param {int[]} nums an array with positive and negative numbers
     # @param {int} k an integer
     # @return {double} the maximum average
@@ -26,6 +26,10 @@ class Solution_OLD:
     def loopDataIndex(self, index, k):
         max_avg = self.calcAvg(index, k)
         for step in range(k, self.data_length - index + 1):
+            tmp = self.data[index + step - 1]
+            if tmp < max_avg: 
+                continue
+
             tmp_avg = self.calcAvg(index, step)
             max_avg = max(max_avg, tmp_avg)
         return max_avg
@@ -40,15 +44,6 @@ class Solution_OLD:
 
 
 solution = Solution()
-result = solution.maxAverage([5,-10,4], 2)
+result = solution.maxAverage([-4,5,-4,5,-4,5,-4,5,-4,5,-4,5,-4,5,-4,5,-4,5,-1000], 10)
 print(result)
 
-
-
-class Solution:
-    # @param {int[]} nums an array with positive and negative numbers
-    # @param {int} k an integer
-    # @return {double} the maximum average
-    def maxAverage(self, nums, k):
-        self.data = nums
-        self.data_length = len(nums)
